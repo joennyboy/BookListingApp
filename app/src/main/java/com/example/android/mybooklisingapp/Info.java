15885e42730ad16b622be5f2c4cd98342e722188
Jonatino <jonatinoversace@gmail.com>
2017-08-12 15:07:57 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/android/mybooklisingapp/Info.java
@@ -0,0 +1,45 @@
+package com.example.android.mybooklisingapp;
+
+/**
+ * Created by Etumusei on 7/8/2017.
+ */
+
+public class Info {
+
+    /**
+     * Name of the author)
+     */
+    public final String author;
+
+    /**
+     * title of the book
+     */
+    public final String title;
+
+    /**
+     * Constructs a new {@link Info}.
+     *
+     * @param infoTitle  is the title of the booklisting
+     * @param infoAuthor is the time name of author of the books
+     */
+    public Info(String infoTitle, String infoAuthor) {
+
+        author = infoAuthor;
+        title = infoTitle;
+
+    }
+    /**
+     * Get the name of the artist
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * Get the title of the song.
+     */
+    public String getTitle() {
+        return title;
+    }
+
+}
